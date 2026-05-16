dce596e1b3fe8076e7c41ed5835bf704439440fa
jameskhele <jkhele@aortem.io>
2026-05-16 13:22:59 +0530

    Remove obsolete backend folder to fix Vercel multi-service error

--- backend/models/Blog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const blogSchema = new mongoose.Schema({
-  slug: String,
-  title: String,
-  content: String
-});
-
-module.exports = mongoose.model("Blog", blogSchema);
